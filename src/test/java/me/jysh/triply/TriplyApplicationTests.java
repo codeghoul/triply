c6fe05714633faea10cf3963a2fb383fbfd3d456
codeghoul <jayesh.patel.2197@gmail.com>
2024-01-30 08:04:15 +0530

    Add initial project files.

---
+++ src/test/java/me/jysh/triply/TriplyApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jysh.triply;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriplyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
