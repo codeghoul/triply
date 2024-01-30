3ae7b9e6d654f52889f6d4e8df977341dbdb5bf1
Jayesh Patel <jayesh.patel.2197@gmail.com>
2024-01-30 08:49:09 +0530

    Add initial project files. (#1)
    * Add initial project files.
    
    * Add dummy API.

---
+++ src/main/java/me/jysh/triply/TriplyApplication.java
@@ -0,0 +1,13 @@
+package me.jysh.triply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriplyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriplyApplication.class, args);
+	}
+
+}
