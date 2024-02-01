8c73516a87bf3b51fdb50f338743453d1b4c33e4
codeghoul <jayesh.patel.2197@gmail.com>
2024-02-01 22:09:25 +0530

    Add java docs.

--- src/main/java/me/jysh/triply/config/PreAuthorize.java
+++ src/main/java/me/jysh/triply/config/PreAuthorize.java
@@ -5,9 +5,19 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * The {@code PreAuthorize} annotation is used to indicate that authorization checks should be
+ * performed before the execution of the annotated method. It allows specifying an array of role
+ * names required for access. This annotation can be applied at the method level.
+ */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface PreAuthorize {
 
+  /**
+   * Specifies an array of role names required for access to the annotated method.
+   *
+   * @return an array of role names required for access
+   */
   String[] withRoles() default {};
 }
--- src/main/java/me/jysh/triply/config/interceptors/AuthorizationInterceptor.java
+++ src/main/java/me/jysh/triply/config/interceptors/AuthorizationInterceptor.java
@@ -11,18 +11,37 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+/**
+ * The {@code AuthorizationInterceptor} class is an implementation of Spring's
+ * {@code HandlerInterceptor} designed for handling authorization checks before allowing the
+ * execution of a request handler method. It checks for the presence of the {@code PreAuthorize}
+ * annotation on the target handler method and verifies if the current user possesses the required
+ * roles specified in the annotation.
+ */
 @Component
 public class AuthorizationInterceptor implements HandlerInterceptor {
 
+  /**
+   * This method is called before the execution of the handler method. It checks for the presence of
+   * {@code PreAuthorize} annotation and validates the required roles.
+   *
+   * @param request  the incoming HTTP request
+   * @param response the HTTP response to be sent
+   * @param handler  the target handler method
+   * @return {@code true} if the user has the required roles or if no authorization check is needed,
+   * {@code false} otherwise
+   * @throws UnauthorizedException if the user doesn't have the required access for this API
+   */
   @Override
-  public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-      throws Exception {
+  public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
+      Object handler) {
     if (handler instanceof HandlerMethod handlerMethod) {
-      final PreAuthorize hasAnnotation = handlerMethod.getMethodAnnotation(PreAuthorize.class);
-      if (hasAnnotation == null || hasAnnotation.withRoles().length == 0) {
+      PreAuthorize annotation = handlerMethod.getMethodAnnotation(PreAuthorize.class);
+      if (annotation == null || annotation.withRoles().length == 0) {
         return true;
       }
-      String[] requiredRoles = hasAnnotation.withRoles();
+
+      String[] requiredRoles = annotation.withRoles();
       if (hasRequiredRoles(requiredRoles)) {
         return true;
       } else {
@@ -33,11 +52,16 @@ public class AuthorizationInterceptor implements HandlerInterceptor {
     return true;
   }
 
+  /**
+   * Checks if the user possesses the required roles.
+   *
+   * @param requiredRoles an array of role names required for access
+   * @return {@code true} if the user has at least one of the required roles, {@code false}
+   * otherwise
+   */
   private boolean hasRequiredRoles(String[] requiredRoles) {
-    final Set<String> roles = SecurityContext.getRoles();
-    if (roles == null || roles.isEmpty()) {
-      return false;
-    }
-    return Arrays.stream(requiredRoles).anyMatch(roles::contains);
+    Set<String> roles = SecurityContext.getRoles();
+    return roles != null && !roles.isEmpty() &&
+        Arrays.stream(requiredRoles).anyMatch(roles::contains);
   }
 }
